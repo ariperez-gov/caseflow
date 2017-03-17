cdb90651f3d484faf6340face35e2e64c0462869
Teja Naraparaju <teja.s.naraparaju@gmail.com>
2017-03-17 17:43:57 -0400

    adding a footer example for the style guide

---
+++ client/app/containers/StyleGuide/styleguide-footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StyleGuideFooter = () => {
+  return (
+    <div>
+      <h2 id="footer">Footer</h2>
+      <p>All of Caseflow Apps feature a minimal footer that contains the text
+        <em> “Built with ♡ by the Digital Service at the VA.”</em> and a <em>
+        “Send Feedback”</em> link.</p>
+      <p>Conveniently, if a developer hover’s over the word <em>“Built” </em>
+      they’ll see a tooltip showing the build date of the app that they are viewing.</p>
+    </div>
+  );
+};
+
+export default StyleGuideFooter;
