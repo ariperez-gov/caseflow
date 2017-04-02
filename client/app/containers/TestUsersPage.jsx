a5935ff1ca616c33aded71b405ba9a52384a1c13
Vovk0x <artem.vovk@va.gov>
2017-04-02 13:10:50 -0400

    funking around

--- client/app/containers/TestUsersPage.jsx
+++ client/app/containers/TestUsersPage.jsx
@@ -3,39 +3,46 @@ import ApiUtil from '../util/ApiUtil';
 import Button from '../components/Button';
 
 export default class TestUsersPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUser: this.props.currentUser
+    constructor(props) {
+	super(props);
+	this.state = {
+	    currentUser: this.props.currentUser,
+	    loading: {
+		demo: false
+	    }
+	}
     }
-  }
 
-  render() {
-    let {
-      users
-    } = this.props;
+    render() {
+	let {
+	    users
+	} = this.props;
 
-    return <div className="sub-page">
-      <ul>
-      {users.map((user) => {
-        return <li>
-          <Button
-            name={user["full_name"]}
-            onClick={this.setRole.bind(this, user)}
-            classNames={["usa-button", "usa-button-outline"]}
-          />
-          {Object.keys(user).map((keyname) => {return user[keyname]}).join(",")}
-        </li>
-      })}
-      </ul>
-    </div>;
-  }
+	return <div className="sub-page">
+    <p>
+	{users.map((user) => {
+             return <div>
+		 <Button
+		     app="demo"
+		     name={user["css_id"]}
+		     onClick={this.setRole.bind(this, user)}
+		     loading={this.state.loading.demo}
+		     classNames={["usa-button", "usa-button-outline"]}
+		     ref={user["station_id"]}
+		 />
+             </div>
+	 })}
+    </p>
+	</div>;
+    }
 
-  setRole = (user) => {
-    ApiUtil.post(`/test/set_user/${user["id"]}`).then(() => {
-      window.location.reload()
-    }, () => {
-      console.log("error!")
-    });
-  };
+    setRole = (user) => {
+	
+	ApiUtil.post(`/test/set-user/${user["id"]}`).then(() => {
+	    window.location.reload()
+	    console.log("DAYUM")
+	}, () => {
+	    console.log("error!")
+	});
+    };
 }
